007d1a15a7c956c324d7a8db2530f248ecba62ec
Jaime Niswonger <jniswonger@keyholesoftware.com>
2023-05-25 21:30:33 -0500

    added metrics

--- src/main/java/com/keyholesoftware/MovieResource.java
+++ src/main/java/com/keyholesoftware/MovieResource.java
@@ -2,6 +2,7 @@ package com.keyholesoftware;
 
 import java.util.List;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.ws.rs.GET;
@@ -10,14 +11,22 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/movies")
+@Produces(MediaType.APPLICATION_JSON)
 public class MovieResource {
 
     @Inject
     EntityManager em;
 
+    private final MeterRegistry registry;
+
+    MovieResource(MeterRegistry registry) {
+        this.registry = registry;
+    }
+
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Movie> movies() {
+    @Path("/nowPlaying")
+    public List<Movie> nowPlaying() {
+        registry.counter("list.movies").increment();
         List<Movie> movies = em.createQuery("from Movie", Movie.class).getResultList();
         return movies;
     }
