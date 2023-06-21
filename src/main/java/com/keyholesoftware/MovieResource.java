051d1341ebe2e11297b53829e36f802e4feaacfd
Jaime Niswonger <jniswonger@keyholesoftware.com>
2023-06-21 09:03:26 -0500

    k8s

--- src/main/java/com/keyholesoftware/MovieResource.java
+++ src/main/java/com/keyholesoftware/MovieResource.java
@@ -1,12 +1,9 @@
 package com.keyholesoftware;
 
 import java.util.List;
-import java.util.Random;
 import java.util.Set;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.faulttolerance.Fallback;
-import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.logging.Logger;
 
@@ -32,17 +29,18 @@ public class MovieResource {
     @Inject
     EntityManager em;
 
-    private final MeterRegistry registry;
+    private final MeterRegistry meterRegistry;
 
-    MovieResource(MeterRegistry registry) {
-        this.registry = registry;
+    MovieResource(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
     }
 
     @GET
     @Path("/nowPlaying")
     @RolesAllowed("user")
+    @CacheResult(cacheName = "nowPlayingCache")
     public List<Movie> nowPlaying() {
-        registry.counter("list.movies").increment();
+        meterRegistry.counter("list.movies").increment();
         List<Movie> movies = em.createQuery("from Movie", Movie.class).getResultList();
         return movies;
     }
@@ -55,23 +53,15 @@ public class MovieResource {
     @Path("/nowPlaying2")
     @RolesAllowed("user")
     @Fallback(fallbackMethod = "nowPlayingFallback")
-    @CacheResult(cacheName = "nowPlayingCache")
+    @CacheResult(cacheName = "nowPlaying2Cache")
     public Set<com.keyholesoftware.rest.client.Movie> nowPlaying2() {
 
-        // maybeFail("MovieResource#nowPlaying2() invocation failed");
-        registry.counter("nowPlaying.counter").increment();
+        meterRegistry.counter("nowPlaying.counter").increment();
         LOGGER.info("MovieResource#nowPlaying2() returning successfully");
 
         NowPlaying nowPlaying = tmdbService.nowPlaying();
         return nowPlaying.results;
     }
-
-    // private void maybeFail(String failureLogMessage) {
-    // if (new Random().nextBoolean()) {
-    // LOGGER.error(failureLogMessage);
-    // throw new RuntimeException("TMDB API call failed.");
-    // }
-    // }
 
     Set<com.keyholesoftware.rest.client.Movie> nowPlayingFallback() {
         return Set.of();
